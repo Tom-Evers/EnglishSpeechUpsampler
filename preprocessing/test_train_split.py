640127c9679538869687c21c8680baf34990515e
tevers <tom.j.evers@gmail.com>
2018-05-16 12:09:35 +0200

    PEP 8 refactoring, slight robustness change

--- preprocessing/test_train_split.py
+++ preprocessing/test_train_split.py
@@ -3,16 +3,6 @@ import csv
 import json
 import numpy as np
 
-settings_file = '../settings/data_settings.json'
-
-settings = json.load(open(settings_file))
-validation_fraction = settings['validation_fraction']
-test_fraction = settings['test_fraction']
-file_dir_base = settings['output_dir_name_base']
-output_dir = settings['output_dir_name_base']
-
-np.random.seed(0)
-
 
 def write_csv(filename, pairs):
     with open(filename, 'wb') as csvfile:
@@ -21,6 +11,13 @@ def write_csv(filename, pairs):
             spamwriter.writerow(pair)
 
 
+settings_file = os.sep.join('..', 'settings', 'data_settings.json')
+settings = json.load(open(settings_file))
+validation_fraction = settings['validation_fraction']
+test_fraction = settings['test_fraction']
+file_dir_base = settings['output_dir_name_base']
+
+output_dir = settings['output_dir_name_base']
 if not os.path.exists(output_dir):
     os.makedirs(output_dir)
 
@@ -31,25 +28,21 @@ file_dir_ds = os.path.join(file_dir_base, 'downsampled_splices')
 for filename in os.listdir(file_dir_truth):
     truth_input_filename = os.path.join(file_dir_truth, filename)
     ds_input_filename = os.path.join(file_dir_ds, filename)
-    if not os.path.isfile(truth_input_filename) or not \
-            os.path.isfile(ds_input_filename):
+    if not os.path.isfile(truth_input_filename) or not os.path.isfile(ds_input_filename):
         continue
     truth_ds_pairs.append([truth_input_filename, ds_input_filename])
 
+np.random.seed(0)
 np.random.shuffle(truth_ds_pairs)
 
 validation_start_index = 0
-validation_end_index = validation_start_index +\
-    int(len(truth_ds_pairs)*validation_fraction)
+validation_end_index = validation_start_index + int(len(truth_ds_pairs) * validation_fraction)
 test_start_index = validation_end_index
-test_end_index = test_start_index +\
-    int(len(truth_ds_pairs)*validation_fraction)
+test_end_index = test_start_index + int(len(truth_ds_pairs) * validation_fraction)
 train_start_index = test_end_index
 
-validation_truth_ds_pairs =\
-    truth_ds_pairs[validation_start_index:validation_end_index]
-write_csv(os.path.join(output_dir, 'validation_files.csv'),
-          validation_truth_ds_pairs)
+validation_truth_ds_pairs = truth_ds_pairs[validation_start_index:validation_end_index]
+write_csv(os.path.join(output_dir, 'validation_files.csv'), validation_truth_ds_pairs)
 
 test_truth_ds_pairs = truth_ds_pairs[test_start_index:test_end_index]
 write_csv(os.path.join(output_dir, 'test_files.csv'), test_truth_ds_pairs)
